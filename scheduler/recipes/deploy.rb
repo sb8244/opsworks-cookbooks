e79a3fb51a7315f2d116b832fa8ff2a21d30f493
timdorr <timdorr@timdorr.com>
2013-06-07 17:34:43 -0400

    On second thought, let's stick with everything in the deploy...

--- scheduler/recipes/deploy.rb
+++ scheduler/recipes/deploy.rb
@@ -2,6 +2,12 @@ Chef::Log.info("Deploying scheduler")
 
 node[:deploy].each do |application, deploy|
 
+  execute "export upstart config" do
+    cwd deploy[:current_path]
+    command "echo 'RAILS_ENV=#{deploy[:rails_env]}' > .env"
+    command "sudo bundle exec foreman export upstart /etc/init --app scheduler --user deploy --log #{deploy[:deploy_to]}/shared/log --procfile deploy/Procfile.scheduler --root #{deploy[:current_path]}"
+  end
+
   service "scheduler" do
     provider Chef::Provider::Service::Upstart
     action :restart
