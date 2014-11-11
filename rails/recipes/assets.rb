7f9c4b56494ef0f417e91ec8ab429a19d1d0a98c
Ben Olive <ben.olive@salesloft.com>
2014-11-11 17:29:20 -0500

    Set environment before compiling assets

--- rails/recipes/assets.rb
+++ rails/recipes/assets.rb
@@ -3,6 +3,7 @@ Chef::Log.info("Precompiling assets...")
 node[:deploy].each do |application, deploy|
   execute "rake assets:precompile" do
     cwd deploy[:current_path]
+    environment "RAILS_ENV" => deploy[:rails_env]
     command "bundle exec rake assets:precompile"
   end
 end
