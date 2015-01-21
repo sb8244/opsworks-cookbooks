34039149d2b18457b4f07011205cb3b11737ae68
Stephen Bussey <steve.bussey@salesloft.com>
2015-01-21 18:10:00 -0500

    only do this on the first layer

--- foreman/recipes/default.rb
+++ foreman/recipes/default.rb
@@ -1,25 +1,24 @@
 Chef::Log.info("Deploying foreman")
 
 node[:deploy].each do |application, deploy|
-  node[:opsworks][:instance][:layers].each do |layer_name|
-    Chef::Log.info("Deploying foreman for #{layer_name}")
+  layer_name = node[:opsworks][:instance][:layers].first
+  Chef::Log.info("Deploying foreman for #{layer_name}")
 
-    service layer_name do
-      provider Chef::Provider::Service::Upstart
-      action :stop
-      only_if { File.exists?("/etc/init/#{layer_name}.conf") && `status #{layer_name}`.include?("start") }
-    end
+  service layer_name do
+    provider Chef::Provider::Service::Upstart
+    action :stop
+    only_if { File.exists?("/etc/init/#{layer_name}.conf") && `status #{layer_name}`.include?("start") }
+  end
 
-    execute "export upstart config" do
-      cwd deploy[:current_path]
-      command "bundle exec foreman export upstart /etc/init --app #{layer_name} --user deploy --log #{deploy[:deploy_to]}/shared/log --procfile deploy/Procfile.#{layer_name} --root #{deploy[:current_path]}"
-      only_if { File.exists?("deploy/Procfile.#{layer_name}") }
-    end
+  execute "export upstart config" do
+    cwd deploy[:current_path]
+    command "bundle exec foreman export upstart /etc/init --app #{layer_name} --user deploy --log #{deploy[:deploy_to]}/shared/log --procfile deploy/Procfile.#{layer_name} --root #{deploy[:current_path]}"
+    only_if { File.exists?("deploy/Procfile.#{layer_name}") }
+  end
 
-    service layer_name do
-      provider Chef::Provider::Service::Upstart
-      action :start
-      only_if { File.exists?("/etc/init/#{layer_name}.conf") && `status #{layer_name}`.include?("stop") }
-    end
+  service layer_name do
+    provider Chef::Provider::Service::Upstart
+    action :start
+    only_if { File.exists?("/etc/init/#{layer_name}.conf") && `status #{layer_name}`.include?("stop") }
   end
 end
