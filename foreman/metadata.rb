72f360c8b205af2636126e4f6bc12ebca17e8928
Stephen Bussey <steve.bussey@salesloft.com>
2015-01-21 13:27:40 -0500

    Create a foreman recipe to start proc file based on layer name

---
+++ foreman/metadata.rb
@@ -0,0 +1,6 @@
+name             'foreman'
+maintainer       'SalesLoft'
+maintainer_email 'developers@salesloft.com'
+license          'All rights reserved'
+description      'Deploy foreman Procfiles as deploy/Procfile.[layer_short_name]'
+version          '1.0.0'
